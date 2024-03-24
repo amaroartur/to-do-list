28619df5dcd4c09a72c6337b60fd29b8210a3ca1
Artura <amaroartur2@gmail.com>
2024-03-24 17:01:47 -0300

    feat: new task input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { Header } from './components/Header'
 import './global.css'
+import { PlusCircle } from '@phosphor-icons/react'
+
+import styles from "./App.module.css"
 
 function App() {
 
   return (
-    <Header />  
+    <div className={styles.wrapper}>
+      <Header />
+
+      <div className={styles.newTaskInput}>
+        <input 
+          type="text" 
+          placeholder='Adicione uma nova tarefa'
+        />
+
+        <button>
+          <strong>Criar</strong>
+          <PlusCircle size={20}/>
+        </button>
+      </div>
+    </div>
   )
 }
 
