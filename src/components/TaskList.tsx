ee85909575edbbec3381d4c14579f303c975f184
Artura <amaroartur2@gmail.com>
2024-03-25 21:49:43 -0300

    feat: list container

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,7 @@
 import styles from "./TaskList.module.css"
 
+import clipboard from "../assets/clipboard.svg"
+
 export function TaskList() {
   return (
     <div className={styles.container}>
@@ -7,11 +9,19 @@ export function TaskList() {
         <strong>
           Tarefas criadas<span>0</span>
         </strong>
+
         <strong>
           Tarefas concluídas<span>0</span>
         </strong>
       </header>
-      <main></main>
+
+      <main>
+        <div className={styles.listContainer}>
+          <img src={clipboard} alt="" />
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </main>
     </div>
   )
 }
